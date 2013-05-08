28586bca021e1f447a78161f35608b656501bbec
Ting Wang <tting.wang@gmail.com>
2013-05-08 21:19:00 +0800

    Add app.js and test.js. Testing using mongoose.

---
+++ test.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+
+    var kittySchema = mongoose.Schema({
+        name: String,
+        age: Number
+    });
+
+    kittySchema.methods.speak = function() {
+        var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+console.log(greeting);
+    };
+
+    var Kitten = mongoose.model('Kitten', kittySchema);
+
+    var silence = Kitten({name: 'silence'});
+
+    var noName = Kitten({name: 'noname', age: 23});
+
+    var saveCb = function(err, silence) {
+        console.log('finish saving ' + silence.name);
+        if (err) {
+            console.log('ops, can not save silence');
+        }
+    }
+    silence.save(saveCb);
+    noName.save(saveCb);
+
+    silence.speak();
+    noName.speak();
+
+});
